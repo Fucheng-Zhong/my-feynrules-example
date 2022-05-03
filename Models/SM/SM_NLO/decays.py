0c5eb64e09779bc980cf8be3dcab1608ae1086ad
Fucheng-Zhong <zhongfch@mail2.sysu.edu.cn>
2022-05-03 15:51:00 +0800

    first commit

---
+++ Models/SM/SM_NLO/decays.py
@@ -0,0 +1,9 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.0.0 for Mac OS X x86 (64-bit) (April 7, 2019)
+# Date: Thu 14 Apr 2022 15:47:56
+
+
+from object_library import all_decays, Decay
+import particles as P
+
+
---
+++ Models/SM/SM_NLO/lorentz.py
@@ -0,0 +1,18 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.0.0 for Mac OS X x86 (64-bit) (April 7, 2019)
+# Date: Thu 14 Apr 2022 15:47:56
+
+
+from object_library import all_lorentz, Lorentz
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+try:
+   import form_factors as ForFac 
+except ImportError:
+   pass
+
+
+1 = Lorentz(name = '1',
+            spins = [],
+            structure = '1')
+
